781bb4eabe0b7e6353c7f5d3a93416423827de85
HarelYoav <yoavbkk@gmail.com>
2023-04-08 19:15:19 +0300

    changed blog page

--- src/screens/blogs/Blogs.tsx
+++ src/screens/blogs/Blogs.tsx
@@ -6,16 +6,16 @@ const Blogs = () => {
   return (
     <Box dir={'rtl'} display={'flex'} flexDirection={'column'} alignItems={'center'}
     minHeight={'40vh'}>
-      <Typography variant='h3' mb={2}>
+      <Typography variant='h4' mb={5}>
         הדף עדיין בבנייה
       </Typography>
       <Card sx={{backgroundColor: 'inherit', color: 'inherit', boxShadow: 'rgba(0, 0, 0, 0.16) 2px 2px 2px 5px'}}>
-        <Box display={'flex'} p={4} justifyContent={'space-between'} gap={2}>
+        <Box p={4} justifyContent={'space-between'} gap={2}>
           <Typography variant='h5'>
             הגדרת CD/CI Pipeline להעלה אוטומטית של אפליקצייית React לשרת AWS ec2.
           </Typography>
           <Link to='jenkins-pipeline' style={{textDecoration: 'none'}}>
-            <Button variant='contained'>
+            <Button variant='contained' size='large' fullWidth={true}>
               צפה 
             </Button>
           </Link>
